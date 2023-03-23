f9823e5cb63eacd14dc7ec9c5d2478c2332fc7dc
Andy Hsu <andy1503hsu@utexas.edu>
2023-03-22 23:12:42 -0500

    Renamed file

--- Andy H/Post Time Interpolation/GetTimeCorrelatedTable.m
+++ Andy H/Post Time Interpolation/GetTimeCorrelatedTable.m
@@ -3,7 +3,7 @@ clc
 close all
 %% Test getImageryData.m
 clc
-experimentName = "baseline08";  % Change this line to "fog16", "baseline01", etc.
+experimentName = "fog16";  % Change this line to "fog16", "baseline01", etc.
 [imageryData, other] = getImageryData(experimentName);
 
 %% Get Transmissometer and Malvern Data
